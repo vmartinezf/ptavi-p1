965010acee2a51813dc6a252d029221677492962
vanessa fernandez martinez <vmartine@zeta40.aulas.gsyc.es>
2015-09-21 10:57:23 +0200

    Añado la suma

--- calc.py
+++ calc.py
@@ -3,8 +3,19 @@
 
 # Vamos a programar una calculadora
 
+import sys
+
 def main():
     pass
 
+def suma (sum1, sum2):
+    return sum1 + sum2
+
 if __name__ == "__main__":
-    main()
+    try:
+        num1 = int (sys.argv[2])
+        num2 = int (sys.argv[3])
+    except ValueError:
+        sys.exit("Tienen que ser números enteros")
+    print (suma(num1, num2))
+
