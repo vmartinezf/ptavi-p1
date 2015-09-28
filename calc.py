d221951959c7b2daf55c1dd4b02e92242dc03377
vanessa fernandez martinez <vmartine@zeta40.aulas.gsyc.es>
2015-09-28 14:18:49 +0200

    calculadora cambio

--- calc.py
+++ calc.py
@@ -8,14 +8,33 @@ import sys
 def main():
     pass
 
+#convierto el operando sea int o float a float
+def numerical_float (operando):
+    return float(operando)
+ 
+
 def suma (sum1, sum2):
     return sum1 + sum2
 
+def resta (rest1, rest2):
+	return rest1 - rest2
+
+
 if __name__ == "__main__":
+    
+    numero1 = sys.argv[1]
+    operacion = sys.argv[2]
+    numero2 = sys.argv[3]
+
     try:
-        num1 = int (sys.argv[2])
-        num2 = int (sys.argv[3])
+        num1 = numerical_float (numero1)
+        num2 = numerical_float (numero2)
     except ValueError:
-        sys.exit("Tienen que ser números enteros")
-    print (suma(num1, num2))
+        sys.exit("Error: Non numerical parameters")
+   
+    if (operacion == "suma"):    
+        print (suma(num1, num2))
+    elif (operacion == "resta"):
+        print (resta(num1, num2))
+
 
